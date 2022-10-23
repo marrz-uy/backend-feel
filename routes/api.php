fa113e97a3d7f49e4da2ea5023a41bc23ebf8002
martinrodfl <martinrodf@gmail.com>
2022-10-23 16:07:02 -0300

    Arreglando cors y return userProfile para userGoogle

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\UserProfileController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
 
 /* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
