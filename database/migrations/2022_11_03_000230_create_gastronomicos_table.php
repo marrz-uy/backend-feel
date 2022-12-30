87099f078ea8dae41f955d97ede624c49a35b93f
martinrodfl <martinrodf@gmail.com>
2022-12-29 21:06:53 -0300

    Se quitan id de categorias-prueba con csv

--- database/migrations/2022_11_03_000230_create_gastronomicos_table.php
+++ database/migrations/2022_11_03_000230_create_gastronomicos_table.php
@@ -14,7 +14,6 @@ class CreateGastronomicosTable extends Migration
     public function up()
     {
         Schema::create('gastronomicos', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('puntosinteres_id')
             ->constrained('puntosinteres')
             ->onUpdate('cascade')
